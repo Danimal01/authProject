975368a2dfe660c02ec8c15b5035d878a2972266
Danimal01 <danialdadkhoo@gmail.com>
2023-07-13 20:08:59 -0400

    Basic Go BackEnd

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/rs/cors"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+	})
+
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:4200"}, // change this to the appropriate front-end URL
+		AllowCredentials: true,
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
+		AllowedHeaders:   []string{"*"},
+	})
+
+	handler := c.Handler(r)
+	addr := ":8080"
+	fmt.Printf("Server listening on %s\n", addr)
+	log.Fatal(http.ListenAndServe(addr, handler))
+}
