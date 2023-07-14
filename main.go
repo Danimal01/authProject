ae718fa2e1b676bbd1dc9dd93705509489e48c64
Danimal01 <danialdadkhoo@gmail.com>
2023-07-13 22:45:29 -0400

    loginRegisterJWTWorking

--- main.go
+++ main.go
@@ -1,24 +1,54 @@
 package main
-//test ok
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/mux"
 	"github.com/rs/cors"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+
 )
 
+var secretKey = "your_secret_key" // Change this to your own secret key
+
+type User struct {
+	gorm.Model
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+// DB connection
+var db *gorm.DB
+
 func main() {
+	// DB initialization
+	var err error
+	dsn := "user=postgres password=Danial dbname=cool sslmode=disable" // Replace with your DB credentials
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println("Failed to connect to the database:", err)
+		panic("Failed to connect to the database!")
+	} else {
+		fmt.Println("Connected to the database successfully")
+	}
+
+	db.AutoMigrate(&User{})
+
 	r := mux.NewRouter()
 
-	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
-	})
+	// User routes
+	r.HandleFunc("/api/register", RegisterUser).Methods("POST")
+	r.HandleFunc("/api/login", LoginUser).Methods("POST")
 
 	c := cors.New(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:4200"}, // change this to the appropriate front-end URL
+		AllowedOrigins:   []string{"http://localhost:4200"},
 		AllowCredentials: true,
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
 		AllowedHeaders:   []string{"*"},
@@ -29,3 +59,58 @@ func main() {
 	fmt.Printf("Server listening on %s\n", addr)
 	log.Fatal(http.ListenAndServe(addr, handler))
 }
+
+// RegisterUser creates a new user
+func RegisterUser(w http.ResponseWriter, r *http.Request) {
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	user.Password = string(hash)
+	db.Create(&user)
+
+	json.NewEncoder(w).Encode(user)
+}
+
+// LoginUser logs in a user and returns a JWT
+func LoginUser(w http.ResponseWriter, r *http.Request) {
+	var user, userDB User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	db.Where("email = ?", user.Email).First(&userDB)
+
+	err = bcrypt.CompareHashAndPassword([]byte(userDB.Password), []byte(user.Password))
+	if err != nil {
+		http.Error(w, "Invalid credentials", http.StatusBadRequest)
+		return
+	}
+
+	expirationTime := time.Now().Add(5 * time.Minute)
+	claims := &jwt.StandardClaims{
+		Subject:   userDB.Email,
+		ExpiresAt: expirationTime.Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString([]byte(secretKey))
+	if err != nil {
+		http.Error(w, "Failed to create a token", http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(map[string]string{"token": tokenString, "email": userDB.Email})
+}
+
